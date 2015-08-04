49bcb20734f3d424d28f2f09bc08207a9866cbd4
uxyheaven <uxyheaven@163.com>
2015-08-04 21:11:47 +0800

    update

--- XYRouter/router/XYRouter.m
+++ XYRouter/router/XYRouter.m
@@ -59,7 +59,7 @@
         [UIApplication sharedApplication].delegate.window.rootViewController = rootViewController;
         [[UIApplication sharedApplication].delegate.window makeKeyAndVisible];
         _rootViewController = rootViewController;
-        if ([rootViewController isKindOfClass:[UINavigationController class]]);
+        if ([rootViewController isKindOfClass:[UINavigationController class]])
         {
             _navigationController = (UINavigationController *)rootViewController;
         }
